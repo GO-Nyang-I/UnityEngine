d1b10970d5a228c17d7ae23a485f67ef1abd7569
hyezz <hyezzx@gmail.com>
2022-09-29 22:17:40 +0900

    [Login] 회원가입 기능 추가

--- GO-Nyang-I/Assets/Identity/Scipts/LoginManager.cs
+++ GO-Nyang-I/Assets/Identity/Scipts/LoginManager.cs
@@ -6,38 +6,76 @@ using UnityEngine.SceneManagement;
 
 using TMPro;
 
+// GameKit
+#if UNITY_EDITOR
+using AWS.GameKit.Editor.Utils;
+#endif
+
+using AWS.GameKit.Common;
+using AWS.GameKit.Runtime.Core;
+using AWS.GameKit.Runtime.Features.GameKitIdentity;
+using AWS.GameKit.Runtime.FeatureUtils;
+using AWS.GameKit.Runtime.Models;
+using AWS.GameKit.Runtime.Utils;
+
 public class LoginManager : MonoBehaviour
 {
     public Button LoginButton;
+    public Button RegistrationButton;
 
-    public TMP_InputField inputId;
-    public TMP_InputField inputPw;
+    private IIdentityProvider _identity;
 
-    private string id = "id";
-    private string pw = "pw";
-
+    public TMP_InputField _userNameInputField;
+    public TMP_InputField _passwordInputField;
+    
     void Start()
     {
-        LoginButton.onClick.AddListener(LoginButtonClicked);
+        _identity = GameKitFeature<Identity>.Get();
+
+        LoginButton.onClick.AddListener(OnLogin);
+        RegistrationButton.onClick.AddListener(OnRegisterUser);
     }
 
-    void LoginButtonClicked()
+    public void OnRegisterUser()
     {
-        if (inputId.text == id && inputPw.text == pw)
+        UserRegistration userRegistration = new UserRegistration
         {
-            Debug.Log("로그인 성공");
-            LoadingSceneController.LoadScene("UIScene");
-        }
-        else
+            UserName = _userNameInputField.text,    
+            Password = _passwordInputField.text,
+            Email = ""
+        };
+
+        _identity.Register(userRegistration, (uint resultCode) =>
         {
-            Debug.Log("로그인 실패");
-        }
-        
+            Debug.Log($"Identity.Register() completed with result code {GameKitErrorConverter.GetErrorName(resultCode)} ({GameKitErrors.ToString(resultCode)}). This result code is explained in the " +
+                      $"Register() method's documentation in the file \"{GameKitPaths.Get().PACKAGES_FULL_PATH}/Runtime/Scripts/Features/Identity/IIdentityProvider.cs\"");
+
+            if (resultCode == GameKitErrors.GAMEKIT_SUCCESS)
+            {
+                Debug.Log("UserRegistration Success");
+            }
+
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnLogin()
     {
-        
+        UserLogin userLoginRequest = new UserLogin()
+        {
+            UserName = _userNameInputField.text,
+            Password = _passwordInputField.text
+        };
+
+        _identity.Login(userLoginRequest, (uint resultCode) =>
+        {
+            Debug.Log($"Identity.Login() completed with result code {GameKitErrorConverter.GetErrorName(resultCode)} ({GameKitErrors.ToString(resultCode)}). This result code is explained in the " +
+                      $"Login() method's documentation in the file \"{GameKitPaths.Get().PACKAGES_FULL_PATH}/Runtime/Scripts/Features/Identity/IIdentityProvider.cs\"");
+
+            if (resultCode == GameKitErrors.GAMEKIT_SUCCESS)
+            {
+                LoadingSceneController.LoadScene("UIScene");
+            }
+        });
     }
+
 }
