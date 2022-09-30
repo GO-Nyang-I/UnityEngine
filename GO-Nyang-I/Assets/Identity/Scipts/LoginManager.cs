193fa0217ddc8d9d1632b83c74c150065bcf1782
Kang Sohyeon <sobokso6ok@gmail.com>
2022-09-30 21:42:34 +0900

    [Login] LoginScene 팝업 표시 추가

--- GO-Nyang-I/Assets/Identity/Scipts/LoginManager.cs
+++ GO-Nyang-I/Assets/Identity/Scipts/LoginManager.cs
@@ -20,28 +20,65 @@ using AWS.GameKit.Runtime.Utils;
 
 public class LoginManager : MonoBehaviour
 {
-    public Button LoginButton;
-    public Button RegistrationButton;
+    // Main 화면 버튼
+    public Button _loginButton;
+    public Button _registButton;
+
+    // Main 화면 TextField
+    public TMP_InputField _loginUserNameInputField;
+    public TMP_InputField _loginPasswordInputField;
+
+    // Popup 창
+    public GameObject _popupSet;
+    public GameObject _signupPopup;
+    public GameObject _existUserPopup;
+
+    // Popup 화면 TextField
+    public TMP_InputField _resisterUserNameInputField;
+    public TMP_InputField _resisterPasswordInputField;
+
+    // Popup 내부 버튼
+    public Button _registConfirmButton;
 
     private IIdentityProvider _identity;
 
-    public TMP_InputField _userNameInputField;
-    public TMP_InputField _passwordInputField;
-    
     void Start()
     {
         _identity = GameKitFeature<Identity>.Get();
 
-        LoginButton.onClick.AddListener(OnLogin);
-        RegistrationButton.onClick.AddListener(OnRegisterUser);
+        _popupSet.SetActive(false);
+        _existUserPopup.SetActive(false);
+        _signupPopup.SetActive(false);
+
+        // 메인 화면의 로그인 버튼을 누르면 OnLogin() 호출
+        _loginButton.onClick.AddListener(OnLogin);
+
+        _registButton.onClick.AddListener(LoadSignupPopup);
+        _registConfirmButton.onClick.AddListener(OnRegisterUser);
+    }
+
+    // 이미 같은 id의 사용자가 존재할 때 팝업 로딩 함수
+    public void LoadExistUserPopup()
+    {
+        _popupSet.SetActive(true);
+        _existUserPopup.SetActive(true);
+        _signupPopup.SetActive(false);
+    }
+
+    // 회원 가입 페이지가 표시되는 팝업 로딩 함수
+    public void LoadSignupPopup ()
+    {
+        _popupSet.SetActive(true);
+        _signupPopup.SetActive(true);
+        _existUserPopup.SetActive(false);
     }
 
     public void OnRegisterUser()
     {
         UserRegistration userRegistration = new UserRegistration
         {
-            UserName = _userNameInputField.text,    
-            Password = _passwordInputField.text,
+            UserName = _resisterUserNameInputField.text,    
+            Password = _resisterPasswordInputField.text,
             Email = ""
         };
 
@@ -62,8 +99,8 @@ public class LoginManager : MonoBehaviour
     {
         UserLogin userLoginRequest = new UserLogin()
         {
-            UserName = _userNameInputField.text,
-            Password = _passwordInputField.text
+            UserName = _loginUserNameInputField.text,
+            Password = _loginPasswordInputField.text
         };
 
         _identity.Login(userLoginRequest, (uint resultCode) =>
