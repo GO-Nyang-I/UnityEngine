e0d54c9e370ac39ffe95162ca31476c15e675306
hyezz <hyezzx@gmail.com>
2022-10-08 00:55:34 +0900

    [Login] 회원가입, 로그인 결과 표시

--- GO-Nyang-I/Assets/Identity/Scipts/LoginManager.cs
+++ GO-Nyang-I/Assets/Identity/Scipts/LoginManager.cs
@@ -31,7 +31,7 @@ public class LoginManager : MonoBehaviour
     // Popup 창
     public GameObject _popupSet;
     public GameObject _signupPopup;
-    public GameObject _existUserPopup;
+    public GameObject _signupResultPopup;
 
     // Popup 화면 TextField
     public TMP_InputField _resisterUserNameInputField;
@@ -40,6 +40,9 @@ public class LoginManager : MonoBehaviour
     // Popup 내부 버튼
     public Button _registConfirmButton;
 
+    // Popup 텍스트
+    public TMPro.TMP_Text _signupResultCode;
+
     private IIdentityProvider _identity;
 
     void Start()
@@ -47,10 +50,10 @@ public class LoginManager : MonoBehaviour
         _identity = GameKitFeature<Identity>.Get();
 
         _popupSet.SetActive(false);
-        _existUserPopup.SetActive(false);
+        _signupResultPopup.SetActive(false);
         _signupPopup.SetActive(false);
 
-        // 메인 홤녀의 Login 버튼을 누를 경우
+        // 메인 화면의 Login 버튼을 누를 경우
         _loginButton.onClick.AddListener(OnLogin);
         // 메인 화면의 Signup 버튼을 누를 경우
         _registButton.onClick.AddListener(LoadSignupPopup);
@@ -58,22 +61,74 @@ public class LoginManager : MonoBehaviour
         _registConfirmButton.onClick.AddListener(OnRegisterUser);
     }
 
-    // 이미 같은 id의 사용자가 존재할 때 팝업 로딩 함수
-    public void LoadExistUserPopup()
-    {
-        _popupSet.SetActive(true);
-        _existUserPopup.SetActive(true);
-        _signupPopup.SetActive(false);
-    }
-
     // 회원 가입 페이지가 표시되는 팝업 로딩 함수
     public void LoadSignupPopup ()
     {
         _popupSet.SetActive(true);
         _signupPopup.SetActive(true);
-        _existUserPopup.SetActive(false);
+        _signupResultPopup.SetActive(false);
+        ClearRegisterUser();
     }
 
+    // 회원가입, 로그인 결과 코드 표시
+    void LoadResultCode()
+    {
+        _popupSet.SetActive(true);
+        _signupPopup.SetActive(false);
+        _signupResultPopup.SetActive(true);
+    }
+
+    public void ClearRegisterUser()
+    {
+        _resisterUserNameInputField.text = "";
+        _resisterPasswordInputField.text = "";
+    }
+
+    public void ClearLoginUser()
+    {
+        _loginUserNameInputField.text = "";
+        _loginPasswordInputField.text = "";
+    }
+
+    // 회원가입 결과 표시
+    void DisplayRegisterResult(uint ResultCode)
+    {
+        if (ResultCode == GameKitErrors.GAMEKIT_SUCCESS)
+        {
+            Debug.Log("UserRegistration Success");
+            _signupResultCode.text = "회원가입에 성공했습니다.";
+        }
+        else if (ResultCode == GameKitErrors.GAMEKIT_ERROR_MALFORMED_PASSWORD)
+        {
+            Debug.Log("UserRegistration Fail: GAMEKIT_ERROR_MALFORMED_PASSWORD");
+            _signupResultCode.text = "비밀번호를 8자 이상\n입력해주세요.";
+        }
+        else if (ResultCode == GameKitErrors.GAMEKIT_ERROR_REGISTER_USER_FAILED)
+        {
+            Debug.Log("UserRegistration Fail: GAMEKIT_ERROR_MALFORMED_USERNAME");
+            _signupResultCode.text = "이미 가입된\n사용자입니다.";
+        }
+        LoadResultCode();
+    }
+
+    // 로그인 결과 표시
+    void DisplayLoginResult(uint ResultCode)
+    {
+        if (ResultCode == GameKitErrors.GAMEKIT_SUCCESS)
+        {
+            Debug.Log("UserLogin Success");
+            LoadingSceneController.LoadScene("UIScene");
+        }
+        else if (ResultCode == GameKitErrors.GAMEKIT_ERROR_LOGIN_FAILED)
+        {
+            Debug.Log("UserLogin Success");
+            _signupResultCode.text = "없는 사용자입니다. \n 회원가입을 해주세요.";
+            ClearLoginUser();
+            LoadResultCode();
+        }
+    }
+
+    // 회원가입
     public void OnRegisterUser()
     {
         UserRegistration userRegistration = new UserRegistration
@@ -88,14 +143,12 @@ public class LoginManager : MonoBehaviour
             Debug.Log($"Identity.Register() completed with result code {GameKitErrorConverter.GetErrorName(resultCode)} ({GameKitErrors.ToString(resultCode)}). This result code is explained in the " +
                       $"Register() method's documentation in the file \"{GameKitPaths.Get().PACKAGES_FULL_PATH}/Runtime/Scripts/Features/Identity/IIdentityProvider.cs\"");
 
-            if (resultCode == GameKitErrors.GAMEKIT_SUCCESS)
-            {
-                Debug.Log("UserRegistration Success");
-            }
+            DisplayRegisterResult(resultCode);
 
         });
     }
 
+    // 로그인
     public void OnLogin()
     {
         UserLogin userLoginRequest = new UserLogin()
@@ -109,10 +162,7 @@ public class LoginManager : MonoBehaviour
             Debug.Log($"Identity.Login() completed with result code {GameKitErrorConverter.GetErrorName(resultCode)} ({GameKitErrors.ToString(resultCode)}). This result code is explained in the " +
                       $"Login() method's documentation in the file \"{GameKitPaths.Get().PACKAGES_FULL_PATH}/Runtime/Scripts/Features/Identity/IIdentityProvider.cs\"");
 
-            if (resultCode == GameKitErrors.GAMEKIT_SUCCESS)
-            {
-                LoadingSceneController.LoadScene("UIScene");
-            }
+            DisplayLoginResult(resultCode);
         });
     }
 
