dc2a98b2b34849d15c9267ee47cc1f937199e360
Kang Sohyeon <sobokso6ok@gmail.com>
2022-10-13 23:11:00 +0900

    [Graphic] 손님 캐릭터 추가 및 시간차로 등장하도록 수정

--- GO-Nyang-I/Assets/Main/Scripts/PlayerController.cs
+++ GO-Nyang-I/Assets/Main/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject Guest1;
+    public GameObject Guest2;
+    public GameObject Guest3;
+
     private Animator animator;
+
+    private float time;
 
     private void Start()
     {
@@ -13,5 +19,23 @@ public class PlayerController : MonoBehaviour
     public void SetMakingAnimaition()
     {
         animator.SetTrigger("Making");
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > 3.0f)
+        {
+            Guest1.gameObject.SetActive(true);
+        }
+        if (time > 12.0f)
+        {
+            Guest2.gameObject.SetActive(true);
+        }
+        if (time > 20.0f)
+        {
+            Guest3.gameObject.SetActive(true);
+        }
     }
 }
